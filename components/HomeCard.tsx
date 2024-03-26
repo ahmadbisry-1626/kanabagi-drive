5e80b5f2e2c51caad46032cb782f521dcce1d4b0
16_shrr <ctbaby1626@gmail.com>
2024-03-26 16:29:35 +0700

    Pagination and Details Button

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -11,41 +11,106 @@ import {
     PaginationNext,
     PaginationPrevious,
 } from "@/components/ui/pagination"
+import { Button } from './ui/button'
+import DetailsButton from './DetailsButton'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
 
 type HomeCardProps = {
     searchTerm: string,
 }
 
+const pageSize = 6;
+
 
 const HomeCard = ({ searchTerm }: HomeCardProps) => {
     const [originalContent, setOriginalContent] = useState(homeCard);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [openDialog, setOpenDialog] = useState(false)
+
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+
     const filteredContent = originalContent.filter(card => {
         return card.name.toLowerCase().includes(searchTerm.toLowerCase());
     });
 
+    const currentData = filteredContent.slice(startIndex, endIndex);
+
+    const handlePageChange = (page: any) => {
+        setCurrentPage(page);
+    };
+
     return (
 
         <section className='flex flex-col items-center w-full'>
-            {filteredContent.length > 0 ? (
+            {currentData.length > 0 ? (
                 <div className='grid md:grid-cols-2 xl:grid-cols-3 grid-cols-1 w-full place-items-center gap-10 md:gap-[50px] pb-10'>
-                    {filteredContent.map((card) => (
-                    <div className='flex flex-col bg-gray-50 rounded-b-[12px] shadow-lg' key={card.id}>
-                        <div className='relative group'>
-                            <Image src={card.imageUrl} alt='' width={400} height={400} className='rounded-t-[12px]' />
-                            <span className='group absolute top-0 h-full w-full bg-black/50 text-white font-medium text-center flex items-center justify-center opacity-0 hover:!opacity-100 rounded-t-[12px] transition duration-200 ease-in-out flex-col'>
-                                <Separator className='w-[100px] opacity-0 group-hover:opacity-100 transition duration-1000 group-hover:translate-x-[20px]' />
-                                <p className='opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out py-2 text-[20px]'>{card.name}</p>
-                                <Separator className='w-[100px] opacity-0 group-hover:opacity-100 transition duration-1000 group-hover:-translate-x-[20px]' />
-                            </span>
-                        </div>
+                    {currentData.map((card) => (
+                        <div className='group flex flex-col bg-gray-50 rounded-b-[12px] shadow-lg' key={card.id} onClick={() => setOpenDialog(true)}>
+                            <div className='relative group'>
+                                <Image src={card.imageUrl} alt='' width={400} height={400} className='rounded-t-[12px]' />
+                                <span className='group absolute top-0 h-full w-full bg-black/50 text-white font-medium text-center flex items-center justify-center opacity-0 hover:!opacity-100 rounded-t-[12px] transition duration-200 ease-in-out flex-col'>
+                                    <Separator className='w-[100px] opacity-0 group-hover:opacity-100 transition duration-1000 group-hover:translate-x-[20px]' />
+                                    <p className='opacity-0 group-hover:opacity-100 transition duration-700 ease-in-out py-2 text-[20px]'>{card.name}</p>
+                                    <Separator className='w-[100px] opacity-0 group-hover:opacity-100 transition duration-1000 group-hover:-translate-x-[20px]' />
+                                </span>
+                            </div>
 
-                        <div className='py-4 px-4 flex justify-between w-full items-center'>
-                            <p className='text-[16px] font-medium'>
-                                {card.description}
-                            </p>
-                            <Image src={card.iconUrl} alt='' width={24} height={24} />
+                            <div className='group-hover:hidden py-4 px-4 flex justify-between w-full items-center'>
+                                <p className='text-[16px] font-medium group-hover:hidden transition duration-200 ease-in-out'>
+                                    {card.file}
+                                </p>
+                                <Image src={card.iconUrl} alt='' width={24} height={24} className='group-hover:hidden' />
+                            </div>
+
+                            <Dialog key={card.id}>
+                                <DialogTrigger className="hidden group-hover:flex w-full items-center justify-center p-4 transition-all duration-200 ease-in-out bg-blue-800 hover:bg-blue-900 rounded-b-[12px]">
+                                    <div className='flex items-center gap-4'>
+                                        <span className='font-medium text-[16px] text-white'>Details</span>
+                                        <Image src="/assets/icons/info.png" alt='' width={20} height={20} />
+                                    </div>
+                                </DialogTrigger>
+                                <DialogContent className=''>
+                                    <DialogHeader className='gap-2'>
+                                        <DialogTitle>{card.name}'s Details</DialogTitle>
+                                        <Separator className='' />
+                                        <div className='flex flex-col max-md:items-center gap-4 md:flex-row'>
+                                            <Image src={card.imageUrl} alt='' width={350} height={350} className='rounded-[12px] md:w-[350] md:h-[350px] w-[250px] h-[250px] bg-contain object-cover' />
+
+                                            <div className='flex flex-col gap-2 md:gap-0'>
+
+                                                <div className='flex md:hidden items-center gap-4 justify-center'>
+                                                    <h1 className='text-[24px] font-semibold'>{card.name}</h1> |
+                                                    <div className='gap-2 flex items-center'>
+                                                        <Image src={card.iconUrl} alt='' width={20} height={20} />
+                                                        <p>{card.file}</p>
+                                                    </div>
+                                                </div>
+
+                                                <h1 className='md:flex hidden text-[24px] font-semibold pb-2'>{card.name}</h1>
+                                                <p className='text-gray-400 text-center md:text-start'>{card.description}</p>
+                                                <div className='hidden md:flex gap-2 absolute bottom-20 items-center'>
+                                                    <Image src={card.iconUrl} alt='' width={20} height={20} />
+                                                    <p>{card.file}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </DialogHeader>
+                                    <DialogFooter>
+                                        <Button type="submit" className='w-full bg-blue-800 hover:bg-blue-900'>Download</Button>
+                                    </DialogFooter>
+                                </DialogContent>
+                            </Dialog>
+                            {/* <DetailsButton style='hidden group-hover:flex' currentData={currentData}/> */}
                         </div>
-                    </div>
                     ))}
                 </div>
             ) : (
@@ -57,19 +122,18 @@ const HomeCard = ({ searchTerm }: HomeCardProps) => {
             <Pagination className='pb-4 pt-4'>
                 <PaginationContent>
                     <PaginationItem>
-                        <PaginationPrevious href="#" />
+                        <PaginationPrevious href="#" onClick={() => handlePageChange(currentPage - 1)} />
                     </PaginationItem>
+                    {/* Loop through pages */}
+                    {Array.from({ length: Math.ceil(homeCard.length / pageSize) }, (_, index) => (
+                        <PaginationItem key={index}>
+                            <PaginationLink href="#" onClick={() => handlePageChange(index + 1)} isActive={index + 1 === currentPage}>
+                                {index + 1}
+                            </PaginationLink>
+                        </PaginationItem>
+                    ))}
                     <PaginationItem>
-                        <PaginationLink href="#" isActive>1</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink href="#">2</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationEllipsis />
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationNext href="#" />
+                        <PaginationNext href="#" onClick={() => handlePageChange(currentPage + 1)} />
                     </PaginationItem>
                 </PaginationContent>
             </Pagination>
--- constants/index.ts
+++ constants/index.ts
@@ -46,62 +46,71 @@ export const homeCard = [
         name: "Yoona",
         imageUrl: "/assets/images/yoona.jpg",
         iconUrl: "/assets/icons/microphone.png",
-        description: "Audio"
+        description: "Im Yoon-ah (Korean: 임윤아; born May 30, 1990), known mononymously as Yoona, is a South Korean singer and actress. After training for five years, she debuted as a member of girl group Girls' Generation (and later its subgroup Girls' Generation-Oh!GG) in August 2007, which went on to become one of the best-selling artists in South Korea.",
+        file: "Audio",
     },
     {
         id: "2",
         name: "Yoona",
         imageUrl: "/assets/images/yoona2.jpg",
         iconUrl: "/assets/icons/camera.png",
-        description: "Photo"
+        description: "Im Yoon-ah (Korean: 임윤아; born May 30, 1990), known mononymously as Yoona, is a South Korean singer and actress. After training for five years, she debuted as a member of girl group Girls' Generation (and later its subgroup Girls' Generation-Oh!GG) in August 2007, which went on to become one of the best-selling artists in South Korea.",
+        file: "Photo",
     },
     {
         id: "3",
         name: "Momo",
         imageUrl: "/assets/images/jihyo.jpg",
         iconUrl: "/assets/icons/film.png",
-        description: "Video"
+        description: "Momo Hirai, better known as Momo, is a Japanese singer and dancer whose career is in South Korea. She is a member of the girl group, Twice, formed by JYP Entertainment. Momo is one of three Twice members who come from Japan.",
+        file: "Video"
     },
     {
         id: "4",
         name: "Bae Suzy",
         imageUrl: "/assets/images/bae.jpg",
         iconUrl: "/assets/icons/camera.png",
-        description: "Photo"
+        description: "Bae Su-ji, better known by her stage name Suzy. Even though Suzy is a stage name, JYP deliberately chose that name so that it could differentiate between the names of other celebrities who are also named Suji, the name Suzy has now stuck and become a legend because of it.",
+        file: "Photo",
     },
     {
         id: "5",
         name: "Irene",
         imageUrl: "/assets/images/irene.jpg",
         iconUrl: "/assets/icons/film.png",
-        description: "Video"
+        description: "Bae Joo Hyun, or better known by her stage name Irene, is a singer, television host, actress and leader of the South Korean female vocal group, Red Velvet. Irene is also a member of the Red Velvet sub-unit - Irene & Seulgi.",
+        file: "Video",
     },
     {
         id: "6",
         name: "IU",
         imageUrl: "/assets/images/iu.jpg",
         iconUrl: "/assets/icons/microphone.png",
-        description: "Audio"
+        description: "Lee Ji-eun or commonly known as IU is a singer-songwriter, producer, model and actress from Korea. His stage name comes from the words 'I and You' which symbolizes that 'we' can become 'one' through music.",
+        file: "Audio",
     },
     {
         id: "7",
         name: "Irene",
         imageUrl: "/assets/images/ireene.jpg",
         iconUrl: "/assets/icons/microphone.png",
-        description: "Audio"
+        description: "Bae Joo Hyun, or better known by her stage name Irene, is a singer, television host, actress and leader of the South Korean female vocal group, Red Velvet. Irene is also a member of the Red Velvet sub-unit - Irene & Seulgi.",
+        file: "Audio",
     },
     {
         id: "8",
         name: "Momo",
         imageUrl: "/assets/images/momo.jpg",
         iconUrl: "/assets/icons/camera.png",
-        description: "Photo"
+        description: "Momo Hirai, better known as Momo, is a Japanese singer and dancer whose career is in South Korea. She is a member of the girl group, Twice, formed by JYP Entertainment. Momo is one of three Twice members who come from Japan.",
+        file: "Photo"
     },
     {
         id: "9",
         name: "Tzuyu",
         imageUrl: "/assets/images/tzuyu.jpg",
         iconUrl: "/assets/icons/camera.png",
-        description: "Photo"
+        description: "Chou Tzu-yu or better known as Tzuyu, is a singer and dancer from Taiwan. She is a member of the South Korean girl group Twice, which was formed by JYP Entertainment through the reality show Sixteen in 2015.",
+        file: "Photo",
     },
 ]
